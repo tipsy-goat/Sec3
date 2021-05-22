853c3e8910643106cdd83a6f403504aa85269e3d
marko <marko@192.168.56.1>
2021-05-22 16:23:27 +0300

    start

---
+++ src/sec3/Sec3.java
@@ -0,0 +1,20 @@
+/*
+ * Markos Asimomytis
+ * icsd11011
+ */
+package sec3;
+
+/**
+ *
+ * @author marko
+ */
+public class Sec3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
